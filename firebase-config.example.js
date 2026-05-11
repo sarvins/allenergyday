f704f026b25cf303b533a331f9b213b3678bf4d3
sarvinsrb <sarvinsarabi@gmail.com>
2026-05-11 09:36:09 +0200

    Move Firebase config to external gitignored file
    Real keys go in firebase-config.js (ignored by git).
    firebase-config.example.js stays in repo as a template.

---
+++ firebase-config.example.js
@@ -0,0 +1,9 @@
+const FIREBASE_CONFIG = {
+  apiKey:            "YOUR_API_KEY",
+  authDomain:        "YOUR_PROJECT.firebaseapp.com",
+  databaseURL:       "https://YOUR_PROJECT-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId:         "YOUR_PROJECT",
+  storageBucket:     "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId:             "YOUR_APP_ID"
+};
